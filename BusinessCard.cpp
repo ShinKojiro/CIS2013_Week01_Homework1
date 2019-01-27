d9f82348858058da467018078c29d02d622caecb
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-01-26 19:48:58 -0600

    Initial commit

---
+++ BusinessCard.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "===========================================================\n";
+    cout << "James Hall\n";
+    cout << "Student, Oklahoma State University - Oklahoma City\n\n";
+
+    cout << "12401 N MacArthur Blvd         p: (405) 229-8067\n";
+    cout << "Oklahoma City, OK 73142        e: james.a.hall@okstate.edu\n";
+    cout << "                               g: github.com/shinkojiro\n";
+    cout << "                               t: @reika\n";
+    cout << "===========================================================";
+    return 0;
+}
