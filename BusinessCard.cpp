65a86dc65a6534436cd695c1b7f0866b72f190d6
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-01-30 04:10:59 -0600

    Updated cout usage

--- BusinessCard.cpp
+++ BusinessCard.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-    cout << "===========================================================\n";
-    cout << "James Hall\n";
-    cout << "Student, Oklahoma State University - Oklahoma City\n\n";
+    cout << "===========================================================\n"
+    << "James Hall\n"
+    << "Student, Oklahoma State University - Oklahoma City\n\n"
 
-    cout << "12401 N MacArthur Blvd         p: (405) 229-8067\n";
-    cout << "Oklahoma City, OK 73142        e: james.a.hall@okstate.edu\n";
-    cout << "                               g: github.com/shinkojiro\n";
-    cout << "                               t: @reika\n";
-    cout << "===========================================================";
+    << "12401 N MacArthur Blvd         p: (405) 229-8067\n"
+    << "Oklahoma City, OK 73142        e: james.a.hall@okstate.edu\n"
+    << "                               g: github.com/shinkojiro\n"
+    << "                               t: @reika\n"
+    << "===========================================================";
     return 0;
 }
