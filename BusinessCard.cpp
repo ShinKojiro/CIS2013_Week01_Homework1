420edd694bbcf2513ce5faea88a9a6f249efa9e0
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-01-30 04:18:55 -0600

    Readability

--- BusinessCard.cpp
+++ BusinessCard.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(){
     cout << "===========================================================\n"
-    << "James Hall\n"
-    << "Student, Oklahoma State University - Oklahoma City\n\n"
+         << "James Hall\n"
+         << "Student, Oklahoma State University - Oklahoma City\n\n"
 
-    << "12401 N MacArthur Blvd         p: (405) 229-8067\n"
-    << "Oklahoma City, OK 73142        e: james.a.hall@okstate.edu\n"
-    << "                               g: github.com/shinkojiro\n"
-    << "                               t: @reika\n"
-    << "===========================================================";
+         << "12401 N MacArthur Blvd         p: (405) 229-8067\n"
+         << "Oklahoma City, OK 73142        e: james.a.hall@okstate.edu\n"
+         << "                               g: github.com/shinkojiro\n"
+         << "                               t: @reika\n"
+         << "===========================================================";
     return 0;
 }
